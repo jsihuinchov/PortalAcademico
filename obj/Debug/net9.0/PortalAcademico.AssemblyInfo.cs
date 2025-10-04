6cc7bc8153011aed29609e787763e130924aaafa
Jimena <jimenacuentaderespaldo@gmail.com>
2025-10-04 17:52:22 -0500

    Pregunta 5 COMPLETA: Panel de Coordinador - CRUD de cursos, gestión de matrículas, autorización por roles, vistas completas

--- obj/Debug/net9.0/PortalAcademico.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortalAcademico.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c303566db7f5a981c25e881a063291b3a258e3d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9fe9545afd4036b5433cc38aebf283ffd012cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
